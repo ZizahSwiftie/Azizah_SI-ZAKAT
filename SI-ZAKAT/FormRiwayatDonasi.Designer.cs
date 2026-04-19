c414150b22788562f8822a8e9d37c68b2ed91d9d
Zizahswiftie <azizahulnha@gmail.com>
2026-04-20 04:16:31 +0700

    Menambahkan FormRiwayatDonasi dengan fitur filter berdasarkan nama donatur menggunakan SQL JOIN

--- SI-ZAKAT/FormRiwayatDonasi.Designer.cs
+++ SI-ZAKAT/FormRiwayatDonasi.Designer.cs
@@ -29,10 +29,10 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.dgvRiwayat = new System.Windows.Forms.DataGridView();
+            this.btnTutup = new System.Windows.Forms.Button();
+            this.cbDonatur = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRiwayat)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -44,44 +44,45 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Nama Donatur";
             // 
-            // textBox1
+            // dgvRiwayat
             // 
-            this.textBox1.Location = new System.Drawing.Point(272, 45);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(259, 26);
-            this.textBox1.TabIndex = 1;
+            this.dgvRiwayat.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRiwayat.Location = new System.Drawing.Point(88, 93);
+            this.dgvRiwayat.Name = "dgvRiwayat";
+            this.dgvRiwayat.RowHeadersWidth = 62;
+            this.dgvRiwayat.RowTemplate.Height = 28;
+            this.dgvRiwayat.Size = new System.Drawing.Size(620, 271);
+            this.dgvRiwayat.TabIndex = 2;
             // 
-            // dataGridView1
+            // btnTutup
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(88, 93);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 62;
-            this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(620, 271);
-            this.dataGridView1.TabIndex = 2;
+            this.btnTutup.Location = new System.Drawing.Point(613, 384);
+            this.btnTutup.Name = "btnTutup";
+            this.btnTutup.Size = new System.Drawing.Size(95, 32);
+            this.btnTutup.TabIndex = 3;
+            this.btnTutup.Text = "Tutup";
+            this.btnTutup.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // cbDonatur
             // 
-            this.button1.Location = new System.Drawing.Point(613, 384);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(95, 32);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Tutup";
-            this.button1.UseVisualStyleBackColor = true;
+            this.cbDonatur.FormattingEnabled = true;
+            this.cbDonatur.Location = new System.Drawing.Point(259, 45);
+            this.cbDonatur.Name = "cbDonatur";
+            this.cbDonatur.Size = new System.Drawing.Size(275, 28);
+            this.cbDonatur.TabIndex = 4;
             // 
             // FormRiwayatDonasi
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.cbDonatur);
+            this.Controls.Add(this.btnTutup);
+            this.Controls.Add(this.dgvRiwayat);
             this.Controls.Add(this.label1);
             this.Name = "FormRiwayatDonasi";
             this.Text = "FormRiwayatDonasi";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRiwayat)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -90,8 +91,8 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dgvRiwayat;
+        private System.Windows.Forms.Button btnTutup;
+        private System.Windows.Forms.ComboBox cbDonatur;
     }
 }

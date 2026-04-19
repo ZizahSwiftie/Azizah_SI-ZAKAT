fcd8d2ea3f8450270b1fa84c3b52a3c0a77426ba
Zizahswiftie <azizahulnha@gmail.com>
2026-04-20 00:48:54 +0700

    perbaikan code form login

--- SI-ZAKAT/FormDashboard.Designer.cs
+++ SI-ZAKAT/FormDashboard.Designer.cs
@@ -28,12 +28,174 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(160, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(644, 81);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(102, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DASHBOARD";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button8);
+            this.panel2.Controls.Add(this.button7);
+            this.panel2.Controls.Add(this.button6);
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Location = new System.Drawing.Point(-3, -2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(165, 452);
+            this.panel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Data Warga";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 119);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(162, 55);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Donasi";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 170);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(162, 51);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Validasi Donasi";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(0, 218);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(165, 55);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Stok Sembako";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(3, 269);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(162, 52);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Penyaluran";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(3, 318);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(162, 53);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Pencarian";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(0, 368);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(165, 55);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Logout";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(3, 30);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(162, 47);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Dashboard";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(179, 117);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(257, 114);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(470, 117);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(310, 114);
+            this.panel4.TabIndex = 3;
+            // 
+            // FormDashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "FormDashboard";
             this.Text = "FormDashboard";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
     }
 }

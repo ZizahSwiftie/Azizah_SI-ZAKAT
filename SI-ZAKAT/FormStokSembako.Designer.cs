d69b76a9b1946cad2350fff1bd02b5e37c4cd8a7
Zizahswiftie <azizahulnha@gmail.com>
2026-05-18 02:58:33 +0700

    Feature: Implementasi view data gudang sembako menggunakan vw_StokSembako

--- SI-ZAKAT/FormStokSembako.Designer.cs
+++ SI-ZAKAT/FormStokSembako.Designer.cs
@@ -31,15 +31,15 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button4 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.txtIDBarang = new System.Windows.Forms.TextBox();
+            this.txtNamaBarang = new System.Windows.Forms.TextBox();
+            this.txtStok = new System.Windows.Forms.TextBox();
+            this.btnSimpan = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.dgvStok = new System.Windows.Forms.DataGridView();
+            this.btnTampilkan = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStok)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -69,92 +69,92 @@
             this.label3.TabIndex = 2;
             this.label3.Text = "Stok";
             // 
-            // textBox1
+            // txtIDBarang
             // 
-            this.textBox1.Location = new System.Drawing.Point(328, 44);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(250, 26);
-            this.textBox1.TabIndex = 3;
+            this.txtIDBarang.Location = new System.Drawing.Point(328, 44);
+            this.txtIDBarang.Name = "txtIDBarang";
+            this.txtIDBarang.Size = new System.Drawing.Size(250, 26);
+            this.txtIDBarang.TabIndex = 3;
             // 
-            // textBox2
+            // txtNamaBarang
             // 
-            this.textBox2.Location = new System.Drawing.Point(328, 94);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(250, 26);
-            this.textBox2.TabIndex = 4;
+            this.txtNamaBarang.Location = new System.Drawing.Point(328, 94);
+            this.txtNamaBarang.Name = "txtNamaBarang";
+            this.txtNamaBarang.Size = new System.Drawing.Size(250, 26);
+            this.txtNamaBarang.TabIndex = 4;
             // 
-            // textBox3
+            // txtStok
             // 
-            this.textBox3.Location = new System.Drawing.Point(328, 155);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(250, 26);
-            this.textBox3.TabIndex = 5;
+            this.txtStok.Location = new System.Drawing.Point(328, 155);
+            this.txtStok.Name = "txtStok";
+            this.txtStok.Size = new System.Drawing.Size(250, 26);
+            this.txtStok.TabIndex = 5;
             // 
-            // button1
+            // btnSimpan
             // 
-            this.button1.Location = new System.Drawing.Point(88, 226);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(119, 34);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Simpan";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnSimpan.Location = new System.Drawing.Point(88, 226);
+            this.btnSimpan.Name = "btnSimpan";
+            this.btnSimpan.Size = new System.Drawing.Size(119, 34);
+            this.btnSimpan.TabIndex = 6;
+            this.btnSimpan.Text = "Simpan";
+            this.btnSimpan.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnUpdate
             // 
-            this.button2.Location = new System.Drawing.Point(228, 226);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(109, 34);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "Update";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnUpdate.Location = new System.Drawing.Point(228, 226);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(109, 34);
+            this.btnUpdate.TabIndex = 7;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnHapus
             // 
-            this.button3.Location = new System.Drawing.Point(363, 226);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(125, 34);
-            this.button3.TabIndex = 8;
-            this.button3.Text = "Hapus";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnHapus.Location = new System.Drawing.Point(363, 226);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(125, 34);
+            this.btnHapus.TabIndex = 8;
+            this.btnHapus.Text = "Hapus";
+            this.btnHapus.UseVisualStyleBackColor = true;
             // 
-            // dataGridView1
+            // dgvStok
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(79, 276);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 62;
-            this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(641, 150);
-            this.dataGridView1.TabIndex = 9;
+            this.dgvStok.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStok.Location = new System.Drawing.Point(79, 276);
+            this.dgvStok.Name = "dgvStok";
+            this.dgvStok.RowHeadersWidth = 62;
+            this.dgvStok.RowTemplate.Height = 28;
+            this.dgvStok.Size = new System.Drawing.Size(641, 150);
+            this.dgvStok.TabIndex = 9;
             // 
-            // button4
+            // btnTampilkan
             // 
-            this.button4.Location = new System.Drawing.Point(546, 226);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(174, 34);
-            this.button4.TabIndex = 10;
-            this.button4.Text = "Tampilkan";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnTampilkan.Location = new System.Drawing.Point(546, 226);
+            this.btnTampilkan.Name = "btnTampilkan";
+            this.btnTampilkan.Size = new System.Drawing.Size(174, 34);
+            this.btnTampilkan.TabIndex = 10;
+            this.btnTampilkan.Text = "Tampilkan";
+            this.btnTampilkan.UseVisualStyleBackColor = true;
             // 
             // FormStokSembako
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnTampilkan);
+            this.Controls.Add(this.dgvStok);
+            this.Controls.Add(this.btnHapus);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSimpan);
+            this.Controls.Add(this.txtStok);
+            this.Controls.Add(this.txtNamaBarang);
+            this.Controls.Add(this.txtIDBarang);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "FormStokSembako";
             this.Text = "FormStokSembako";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStok)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -165,13 +165,13 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox txtIDBarang;
+        private System.Windows.Forms.TextBox txtNamaBarang;
+        private System.Windows.Forms.TextBox txtStok;
+        private System.Windows.Forms.Button btnSimpan;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnHapus;
+        private System.Windows.Forms.DataGridView dgvStok;
+        private System.Windows.Forms.Button btnTampilkan;
     }
 }
